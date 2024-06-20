7355b6c7de1d46c1a6579a06b457051cd3d3c06c
Mahim Yadav <gjeetu0764@gmail.com>
2024-06-20 14:27:26 +0530

    Dice-Roll App created

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'; 
+import './App.css';
+import RollDice from './Components/RollDice'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas) 
+
+function App() {
+
+    return ( 
+      <div> 
+            <RollDice /> 
+      </div> 
+  );
+
+}
+
+export default App;
